02276fbfc284403d7abe3f2de7ed0c6045d18ff1
Andreas Nonslid Håvardsen <andreas.nonshaav@hotmail.com>
2023-05-30 16:22:38 +0200

    chore: update for ubuntu install

--- linux/ubuntu.sh
+++ linux/ubuntu.sh
@@ -1,37 +1,67 @@
 #!/bin/bash
 set -ex
 
+sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git curl build-essential make automake cmake ninja-build libgmp-dev libmpfr-dev libmpc-dev flex bison texinfo unzip wget jq fzf bat ripgrep silversearcher-ag zoxide tldr secure-delete openssh-client tmux xclip libgit2-dev exa
 
-if ! command -v pyenv &>/dev/null; then
+# Essential libraries and development tools
+sudo apt install -y \
+    git git-lfs curl build-essential make automake cmake ninja-build \
+    libgmp-dev libmpfr-dev libmpc-dev flex bison texinfo unzip wget jq \
+    fzf bat ripgrep silversearcher-ag zoxide tldr secure-delete openssh-client \
+    tmux xclip libgit2-dev eza software-properties-common neovim \
+    libbz2-dev libncurses5-dev libncursesw5-dev libreadline-dev \
+    libsqlite3-dev libffi-dev liblzma-dev tk-dev zlib1g-dev golang
+
+sudo locale-gen en_US.UTF-8
+sudo update-locale
+
+# Install pyenv only if directory is missing
+if [ ! -d "$HOME/.pyenv" ]; then
   curl https://pyenv.run | bash
-  export PATH="$HOME/.pyenv/bin:$PATH"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 fi
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
 
-if ! command -v nvm &>/dev/null; then
+# Install nvm if not already installed
+if [ ! -d "$HOME/.nvm" ]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-if ! command -v rustup &>/dev/null; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  export PATH="$HOME/.cargo/bin:$PATH"
-fi
-
+# Install Node.js LTS and make sure node is available for Neovim
 nvm install --lts
 nvm use --lts
-npm install --global xpm typescript
+npm install --global xpm typescript neovim
+
+# Install rustup if not already installed
+if ! command -v rustup &>/dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Python installations
 pyenv install -s 3.8.12
 pyenv install -s 3.13.0
 pyenv global 3.13.0
-cargo install fd-find zellij tealdeer
-sudo apt install -y golang
+
+# Rust packages
+cargo install fd-find zellij tealdeer tree-sitter-cli
+
+# Starship prompt
 curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+# NVIM latest version install
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo rm nvim-linux-x86_64.tar.gz
+
+# Cleanup
 sudo apt autoremove -y
-echo "All version managers and tools installed. Use pyenv, nvm, rustup, and xpm for further tool management."
+
 set +ex
+echo "All tools installed. Use pyenv, nvm, rustup, and xpm for further management."
--- symlink.py
+++ symlink.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import shutil
 from pathlib import Path
 
 EXCLUDE = {".git", ".DS_Store", "__pycache__", "LICENSE", "README.md", ".gitignore", ".gitmodules", ".gitattributes"}
@@ -8,52 +8,53 @@ def create_symlink(target, link_name):
     try:
         target = os.path.abspath(os.path.expanduser(target))
         link_name = os.path.abspath(os.path.expanduser(link_name))
-        if os.path.islink(link_name):
-            existing_target = os.readlink(link_name)
-            if os.path.abspath(existing_target) == target:
-                print(f"Symlink already exists and is correct: {link_name} -> {target}")
-                return
+        if os.path.islink(link_name) or os.path.exists(link_name):
+            print(f"Removing existing: {link_name}")
+            if os.path.isdir(link_name) and not os.path.islink(link_name):
+                shutil.rmtree(link_name)
             else:
-                print(f"Symlink exists but points to a different target: {link_name} -> {existing_target}, updating...")
                 os.unlink(link_name)
-        elif os.path.exists(link_name):
-            print(f"Error: A file or directory already exists at {link_name}, cannot create symlink.")
-            return
-        if os.name == "nt":
-            if os.path.isdir(target):
-                os.symlink(target, link_name, target_is_directory=True)
-            else:
-                os.symlink(target, link_name)
-        else:
-            os.symlink(target, link_name)
+        parent_dir = os.path.dirname(link_name)
+        os.makedirs(parent_dir, exist_ok=True)
+        os.symlink(target, link_name)
         print(f"Created symlink: {link_name} -> {target}")
     except Exception as e:
-        print(f"Failed to create/update symlink from {target} to {link_name}: {e}")
+        print(f"Failed to create symlink from {target} to {link_name}: {e}")
+
+def symlink_dir_contents(source_dir, target_dir):
+    for item in os.listdir(source_dir):
+        if item in EXCLUDE:
+            continue
+        src_path = os.path.join(source_dir, item)
+        tgt_path = os.path.join(target_dir, item)
+        create_symlink(src_path, tgt_path)
 
 def main():
     repo_root = Path(__file__).parent.resolve()
     home = Path.home()
     is_windows = os.name == "nt"
 
-    # bashrc
+    # bashrc contents
     bashrc_source = repo_root / "bashrc"
-    bashrc_target = home / "bashrc"
-    if bashrc_source.exists():
-        create_symlink(str(bashrc_source), str(bashrc_target))
+    bashrc_target = home
+    if bashrc_source.exists() and bashrc_source.is_dir():
+        symlink_dir_contents(str(bashrc_source), str(bashrc_target))
     else:
-        print(f"bashrc source not found: {bashrc_source}")
+        print(f"bashrc source directory not found: {bashrc_source}")
 
-    # nvim
+    # nvim contents
     nvim_source = repo_root / "nvim"
     if is_windows:
         nvim_target = Path(os.environ.get("LOCALAPPDATA", home / "AppData/Local")) / "nvim"
     else:
         nvim_target = home / ".config" / "nvim"
-    if nvim_source.exists():
-        nvim_target.parent.mkdir(parents=True, exist_ok=True)
-        create_symlink(str(nvim_source), str(nvim_target))
+
+    if nvim_source.exists() and nvim_source.is_dir():
+        nvim_target.mkdir(parents=True, exist_ok=True)
+        symlink_dir_contents(str(nvim_source), str(nvim_target))
     else:
-        print(f"nvim source not found: {nvim_source}")
+        print(f"nvim source directory not found: {nvim_source}")
 
 if __name__ == "__main__":
     main()
+
