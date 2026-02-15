00ea082441c415985c1fcd33aac38e69f02ed116
Andreas Nonslid Håvardsen <andreas.nonshaav@hotmail.com>
2026-02-15 16:24:03 +0100

    chore(nvim): remove nvim-autopairs for mini.pairs

--- nvim/lua/plugins/small-utils.lua
+++ nvim/lua/plugins/small-utils.lua
@@ -6,43 +6,6 @@ return {
     event = "VeryLazy",
   },
 
-  -- Auto close brackets, quotes, etc.
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {
-      check_ts = true,
-      ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-        java = false,
-      },
-      disable_filetype = { "TelescopePrompt", "vim" },
-      fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = [=[[%'%"%)%>%]%)%}%,]]=],
-        offset = 0,
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-      },
-    },
-    config = function(_, opts)
-      require("nvim-autopairs").setup(opts)
-      
-      -- Integration with completion system (LazyVim handles this automatically)
-      -- Only set up if cmp is available
-      local ok, cmp = pcall(require, "cmp")
-      if ok then
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      end
-    end,
-  },
-
   -- Indent guides - LazyVim has this built-in, but if you want explicit control:
   {
     "lukas-reineke/indent-blankline.nvim",
